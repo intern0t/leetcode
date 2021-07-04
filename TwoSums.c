d9991e5a389fdce4326518e4aeb68da44dd3c32f
Prashant Shrestha <contact@prashant.me>
2021-07-04 10:57:28 -0400

    Adds InsertionSort and LinkedList. Working on MergeSort.

--- TwoSums.c
+++ TwoSums.c
@@ -1,34 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int* twoSum(int* nums, int numsSize, int target) {
-	int* answers = malloc(sizeof(int) * 2);
-	answers[0] = answers[1] = -1;
-	
-	for (int i = 0, j = i + 1; i < numsSize - 1; i++) {
-		int sum = nums[i] + nums[j];
+int *twoSum(int *nums, int numsSize, int target) {
+  int *answers = malloc(sizeof(int) * 2);
+  answers[0] = answers[1] = -1;
 
-		if (sum == target) {
-			answers[0] = i;
-			answers[1] = j;
-			break;
-		} else {
-			j++;
-		}
-	}
+  for (int i = 0, j = i + 1; i < numsSize - 1; i++) {
+    int sum = nums[i] + nums[j];
 
-	return answers;
+    if (sum == target) {
+      answers[0] = i;
+      answers[1] = j;
+      break;
+    } else {
+      j++;
+    }
+  }
+
+  return answers;
 }
 
 int main(void) {
-	int nums[]   = {3,2,3};
-	int target   = 6;
-	int numsSize = sizeof nums / sizeof *nums;
-	int* sums    = twoSum(nums, numsSize, target);
-	if (sums[0] == -1 && sums[1] == -1) {
-		printf("not found!\n");
-	} else {
-		printf("found @ %d, %d = %d\n", sums[0], sums[1], target);
-	}
-	return 0;
+  int nums[] = {3, 2, 3};
+  int target = 6;
+  int numsSize = sizeof nums / sizeof *nums;
+  int *sums = twoSum(nums, numsSize, target);
+  if (sums[0] == -1 && sums[1] == -1) {
+    printf("not found!\n");
+  } else {
+    printf("found @ %d, %d = %d\n", sums[0], sums[1], target);
+  }
+  return 0;
 }
