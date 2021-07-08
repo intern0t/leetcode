c71989965de8d580b06747b9c33b755bb19feb8c
Prashant Shrestha <contact@prashant.me>
2021-07-07 22:30:35 -0400

    Indent Fixtures.

--- LongestCommonPrefix.java
+++ LongestCommonPrefix.java
@@ -4,42 +4,44 @@ import java.util.Arrays;
 
 public class LongestCommonPrefix {
 
-  public static void main(String[] args) {
-    String tests[][] = {
-      { "flower", "flow", "flight" },
-      { "dog", "racecar", "car" },
-      { "abab", "aba", "abc" },
-    };
+    public static void main(String[] args) {
+        String tests[][] = {
+            { "flower", "flow", "flight" },
+            { "dog", "racecar", "car" },
+            { "abab", "aba", "abc" },
+        };
 
-    for (int i = 0; i < tests.length; i++) {
-      for (int j = 0; j < tests[i].length; j++) {
-        System.out.print(tests[i][j] + (j == tests[i].length - 1 ? "" : ", "));
-      }
-      System.out.println(" => " + longestCommonPrefix(tests[i]));
-    }
-  }
-
-  public static String longestCommonPrefix(String[] strs) {
-    if (strs.length == 0 || strs == null) {
-      return "";
+        for (int i = 0; i < tests.length; i++) {
+            for (int j = 0; j < tests[i].length; j++) {
+                System.out.print(
+                    tests[i][j] + (j == tests[i].length - 1 ? "" : ", ")
+                );
+            }
+            System.out.println(" => " + longestCommonPrefix(tests[i]));
+        }
     }
 
-    // Sort by length, **lexicographical sort is necessary + important!
-    Arrays.sort(strs);
+    public static String longestCommonPrefix(String[] strs) {
+        if (strs.length == 0 || strs == null) {
+            return "";
+        }
 
-    // Track the index of a smallest string.
-    int index = 0;
+        // Sort by length, **lexicographical sort is necessary + important!
+        Arrays.sort(strs);
 
-    // Compare the characters at the same string with the last string.
-    while (index < strs[0].length()) {
-      if (strs[0].charAt(index) == strs[strs.length - 1].charAt(index)) {
-        index++;
-      } else {
-        break;
-      }
+        // Track the index of a smallest string.
+        int index = 0;
+
+        // Compare the characters at the same string with the last string.
+        while (index < strs[0].length()) {
+            if (strs[0].charAt(index) == strs[strs.length - 1].charAt(index)) {
+                index++;
+            } else {
+                break;
+            }
+        }
+
+        // If index did not move ( = 0), return empty else return the substring upto index - 1.
+        return index == 0 ? "" : strs[0].substring(0, index);
     }
-
-    // If index did not move ( = 0), return empty else return the substring upto index - 1.
-    return index == 0 ? "" : strs[0].substring(0, index);
-  }
 }
--- MedianofTwoSortedArrays.java
+++ MedianofTwoSortedArrays.java
@@ -2,14 +2,14 @@
 
 public class MedianofTwoSortedArrays {
 
-  public static void main(String[] args) {
-    int testCases[][] = { { 1, 3 }, { 2 }, { 1, 2 }, { 3, 4 } };
-    for(int i = 0; i < testCases.length; i += 2){
-        // System.out.println(testCases[i][])
+    public static void main(String[] args) {
+        int testCases[][] = { { 1, 3 }, { 2 }, { 1, 2 }, { 3, 4 } };
+        for (int i = 0; i < testCases.length; i += 2) {
+            // System.out.println(testCases[i][])
+        }
     }
-  }
 
-  static double findMedianSortedArrays(int[] nums1, int[] nums2) {
-    return 0.0;
-  }
+    static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        return 0.0;
+    }
 }
